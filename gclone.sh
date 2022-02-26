2012e69a56b9b0a6feebcc9cddf596acf9cb78ff
xieshang <30817148+Xiefengshang@users.noreply.github.com>
2022-02-26 12:51:24 +0800

    Create gclone.sh

---
+++ gclone.sh
@@ -0,0 +1,7 @@
+apt install wget -y
+apt install unzip -y
+wget https://github.com/dogbutcat/gclone/releases/download/$(curl -sL https://api.github.com/repos/dogbutcat/gclone/releases | jq -r ".[0].tag_name")/gclone-$(curl -sL https://api.github.com/repos/dogbutcat/gclone/releases | jq -r ".[0].tag_name")-`uname -s`-`dpkg --print-architecture`.zip -O gclone.zip
+unzip gclone.zip
+mv ./gclone*/ gclone
+mv ./gclone/gclone /usr/bin/gclone
+chmod 777 /usr/bin/gclone
