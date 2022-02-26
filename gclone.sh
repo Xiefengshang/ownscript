754c631f5853bd2d9f8598e52cee3b7e3d313db7
xieshang <30817148+Xiefengshang@users.noreply.github.com>
2022-02-26 12:54:40 +0800

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -1,5 +1,6 @@
 apt install wget -y
 apt install unzip -y
+apt install jq -y
 wget https://github.com/dogbutcat/gclone/releases/download/$(curl -sL https://api.github.com/repos/dogbutcat/gclone/releases | jq -r ".[0].tag_name")/gclone-$(curl -sL https://api.github.com/repos/dogbutcat/gclone/releases | jq -r ".[0].tag_name")-`uname -s`-`dpkg --print-architecture`.zip -O gclone.zip
 unzip gclone.zip
 mv ./gclone*/ gclone
