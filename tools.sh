1a3a816a326adab0d3392d907a6d535bace4f5e5
None <30817148+Xiefengshang@users.noreply.github.com>
2024-03-24 20:47:07 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -160,7 +160,7 @@ fi
 }
 
 Update_Shell(){
-  wget -N "http://sh.nekoneko.cloud/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
+  wget -N "https://raw.githubusercontent.com/Xiefengshang/ownscript/main/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
 }
 
 get_opsy() {
